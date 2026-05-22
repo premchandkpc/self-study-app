94b10145600aafd46c925cb57cf7ba486a483736
premchandkpc <premchand.karnakanti@gmail.com>
2026-05-22 13:02:17 +0530

    all

--- react-stateless-app/packages/web/src/components/PageShell.jsx
+++
@@ -1,9 +0,0 @@
-import { createElement as h } from 'react';
-import Navbar from './Navbar.jsx';
-
-export default function PageShell({ topics, currentTopic, children }) {
-  return h('div', { className: 'app-shell' },
-    h(Navbar, { topics, currentTopic }),
-    h('main', { className: 'app-main' }, children),
-  );
-}
