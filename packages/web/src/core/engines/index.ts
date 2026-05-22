0e242c02e2dc8a0969dd7d1069c34a4584ef0038
premchandkpc <premchand.karnakanti@gmail.com>
2026-05-22 23:21:37 +0530

    done

--- packages/web/src/core/engines/index.ts
+++
@@ -1,4 +0,0 @@
-// Core Engines - Beyond basic runtime
-
-export { CognitiveRenderingEngine } from './CognitiveRenderingEngine';
-export type { CognitiveLevel, CognitiveConfig, RevealedScene } from './CognitiveRenderingEngine';
